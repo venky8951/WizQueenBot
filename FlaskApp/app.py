ac9b5238be954995a943d08b6461488fa0c2378d
Sushma <sushmagollapudi22@gmail.com>
2023-05-23 23:24:51 +0530

    Fixed Wiz's mistake

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from source_configurator.OI_filter import OIFilter
+from trade_executor.jugaad_trade_executor import JugaadTradeExecutor
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Flask!'
+    filtered_stocks= OIFilter.filter()
+    for stock in filtered_stocks:
+        JugaadTradeExecutor.execute(stock,any)
+
+
+
+
 app.run(port=5001)
