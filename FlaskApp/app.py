9c5fb41e89d576eb032c7c91f4bb895ef4d257b4
Sushma <sushmagollapudi22@gmail.com>
2023-05-24 15:57:47 +0530

    Revert : Test deployment

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from source_configurator.OI_filter import OIFilter
 from trade_executor.jugaad_trade_executor import JugaadTradeExecutor
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
@@ -12,5 +13,11 @@ def hello():
     return filtered_stocks
 
 
+def test():
+    print("invoked")
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(test,'interval',minutes=10)
+sched.start()
 
 app.run(port=5001)
